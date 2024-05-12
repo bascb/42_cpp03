eaf9478924c8bf35c08347e81f9a63b47a9a0e62
Bernardo Castelo Branco <bcastelo@c2r10s1.42lisboa.com>
2024-05-12 20:03:14 +0100

    One more test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 17:29:29 by bcastelo          #+#    #+#             */
-/*   Updated: 2024/03/03 20:03:00 by bcastelo         ###   ########.fr       */
+/*   Updated: 2024/05/12 20:02:23 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,10 @@ int	main( void )
 	soldier1.takeDamage(5);
 	soldier1.beRepaired(10);
 	soldier2.beRepaired(10);
+	for (int i = 0; i < 5; i++)
+	{
+		soldier1.beRepaired(10);
+		soldier1.attack("Loser");
+	}
 	return (0);
 }

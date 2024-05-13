73299d2b23cd5dc0dcd1c9601896ba881370ba6b
Bernardo Castelo Branco <bcastelo@c2r9s1.42lisboa.com>
2024-05-13 18:48:57 +0100

    Turnning Main tests more legible

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 17:29:29 by bcastelo          #+#    #+#             */
-/*   Updated: 2024/05/12 20:53:59 by bcastelo         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:46:23 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,28 @@
 
 int	main( void )
 {
+	std::cout << "****** ClapTrap construction *******" << std::endl;
 	ClapTrap	soldier1("Brave");
 	ClapTrap	soldier2("Loser");
+	std::cout << "****** ScavTrap construction ************" << std::endl;
 	ScavTrap	der1("Different");
 	ScavTrap	der2( der1 );
 	ScavTrap	der3("Assign");
-	
+	std::cout << "********* ScavTrap copy assignment ***********" << std::endl;
 	der2 = der3;
+	std::cout << "********* ClapTrap war ***********" << std::endl;
 	soldier1.attack("Loser");
 	soldier2.takeDamage(10);
 	soldier1.takeDamage(5);
 	soldier1.beRepaired(10);
 	soldier2.beRepaired(10);
+	std::cout << "********** ScavTrap war *********" << std::endl;
 	der1.attack("Another loser");
 	der1.guardGate();
 	der1.takeDamage(10);
 	der2.takeDamage(20);
 	der3.takeDamage(30);
 	der1.beRepaired(5);
+	std::cout << "****************** Destructors time **************" << std::endl;
 	return (0);
 }
